61ff53a8ad8597e7e9514bb0b5f059614c487b10
khurshid28 <khurshidi2827@gmail.com>
2025-10-20 17:10:27 +0500

    premium nasiya

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -3,7 +3,16 @@ export default function Head() {
     <>
       <title> Premium Nasiya - savdo platformasi</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Premium Nasiya - savdo platformasi" />
+      <meta
+        name="description"
+        content="Premium Nasiya orqali O‘zbekistondagi do‘konlarda tovar va xizmatlarni muddatli to‘lov orqali xarid qiling."
+      />
+
+      <meta
+        name="keywords"
+        content="premium nasiya, nasiya, premium,  tovar, xizmat, muddatli to‘lov, to'lov,  to‘lov, o‘zbekiston"
+      />
+
       <link rel="icon" href="/images/premiumnasiya.ico" />
     </>
   );
