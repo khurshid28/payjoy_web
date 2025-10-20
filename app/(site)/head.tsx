877d345dac3590c244dd0c528706980d0a9cbb91
khurshid28 <khurshidi2827@gmail.com>
2025-10-20 17:31:34 +0500

    premium nasiya

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -3,7 +3,15 @@ export default function Head() {
     <>
       <title> Premium Nasiya - savdo platformasi</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="google-site-verification" content="QNMcACjd1_T1nirEiGNIANzfrXuHlQuFRTCISPcsOO0" />
+      <meta
+        name="google-site-verification"
+        content="QNMcACjd1_T1nirEiGNIANzfrXuHlQuFRTCISPcsOO0"
+      />
+
+      <link rel="icon" href="/favicon.ico?v=2" />
+      <link rel="shortcut icon" href="/favicon.ico?v=2" />
+      <link rel="apple-touch-icon" href="/favicon.ico?v=2" />
+
       <meta
         name="description"
         content="Premium Nasiya orqali O‘zbekistondagi do‘konlarda tovar va xizmatlarni muddatli to‘lov orqali xarid qiling."
