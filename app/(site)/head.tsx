2d680ab7b7439bdcfb8f6fde223e0e4fbf57611a
khurshid28 <khurshidi2827@gmail.com>
2025-10-21 22:55:29 +0500

    premium nasiya

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -8,11 +8,26 @@ export default function Head() {
         content="QNMcACjd1_T1nirEiGNIANzfrXuHlQuFRTCISPcsOO0"
       />
 
-      <link rel="icon" href="/favicon.ico?v=3" />
+      {/* <link rel="icon" href="/favicon.ico?v=3" />
         <link rel="icon" type="image/png" href="/favicon.png?v=3" />
 
       <link rel="shortcut icon" href="/favicon.ico?v=3" />
-      <link rel="apple-touch-icon" href="/favicon.png?v=3" />
+      <link rel="apple-touch-icon" href="/favicon.png?v=3" /> */}
+
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
 
       <meta
         name="description"
